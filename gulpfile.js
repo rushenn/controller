0036da8958172340db93b18d7009cabccfc79b73
Krishna Srinivas <krishna.srinivas@gmail.com>
2015-08-28 00:04:38 -0700

    Remove bloat from package.json and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,164 +1,43 @@
-var gulp          = require('gulp');
+var gulp = require('gulp');
+var del = require('del');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
 
-var $             = require('gulp-load-plugins')();
-var del           = require('del');
-var source        = require('vinyl-source-stream');
-var browserify    = require('browserify');
-var preprocessify = require('preprocessify');
-var runSequence   = require('run-sequence');
-var domain        = require('domain');
-var less          = require('gulp-less');
-
-var env           = 'dev';
-var webserver     = false;
-
-log = function(task, start) {
-  if (!start) {
-    setTimeout(function() {
-      $.util.log('Starting', '\'' + $.util.colors.cyan(task) + '\'...');
-    }, 1);
-  } else {
-    var time = ((new Date() - start) / 1000).toFixed(2) + ' s';
-    $.util.log('Finished', '\'' + $.util.colors.cyan(task) + '\'', 'after', $.util.colors.magenta(time));
-  }
-};
-
-gulp.task('clean:dev', function() {
-  return del(['.tmp']);
-});
-
-gulp.task('clean:dist', function() {
+gulp.task('clean', function() {
   return del(['dist']);
-});
-
-gulp.task('scripts', function() {
-  var dev = env === 'dev';
-  var filePath = './app/scripts/app.js';
-  var extensions = ['.jsx'];
-
-  var bundle = function() {
-    if (dev) {
-      var start = new Date();
-      log('scripts:bundle');
-    }
-    browserify({
-      entries: [filePath],
-      extensions: extensions,
-      debug: env === 'dev'
-    }).transform(preprocessify({
-      env: env
-    }, {
-      includeExtensions: extensions
-    })).transform('reactify')
-    .bundle()
-      .pipe(source('app.js'))
-      .pipe(gulp.dest('.tmp/scripts/bundle'))
-      .pipe($.if(dev, $.tap(function() {
-        log('scripts:bundle', start);
-        if (!webserver) {
-          runSequence('webserver');
-        }
-      })));
-  }
-
-  if (dev) {
-    gulp.src(filePath)
-      .pipe($.plumber())
-      .pipe($.tap(function(file) {
-        var d = domain.create();
-
-        d.on('error', function(err) {
-          $.util.log($.util.colors.red('Browserify compile error:'), err.message, '\n\t', $.util.colors.cyan('in file'), file.path);
-          $.util.beep();
-        });
-
-        d.run(bundle);
-      }));
-  } else {
-    bundle();
-  }
 });
 
 gulp.task('less', function () {
   gulp.src('./app/styles/less/app.less') //path to your main less file
     .pipe(less())
-    .pipe(gulp.dest('./app/styles')); // your output folder
+    .pipe(minifycss())
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('dist'))
 });
 
-gulp.task('imagemin', function() {
-  return gulp.src('app/images/*')
-    .pipe($.imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}]
-    }))
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('copy', function() {
-  return gulp.src(['app/*.txt', 'app/*.ico'])
-    .pipe(gulp.dest('dist'));
+gulp.task('fonts', function() {
+  gulp.src('app/fonts/**/*', {base:"app"})
+    .pipe(gulp.dest('dist'))
 })
 
-gulp.task('bundle', function () {
-  var assets = $.useref.assets();
-  var revAll = new $.revAll({dontRenameFile: [/^\/favicon.ico$/g, '.html']});
-  var jsFilter = $.filter(['**/*.js']);
-  var cssFilter = $.filter(['**/*.css']);
-  var htmlFilter = $.filter(['*.html']);
-
-  return gulp.src('app/index.html')
-    .pipe($.preprocess())
-    .pipe(assets)
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe(jsFilter)
-    .pipe($.uglify())
-    .pipe(jsFilter.restore())
-    .pipe(cssFilter)
-    .pipe($.autoprefixer({
-      browsers: ['last 5 versions']
-    }))
-    .pipe($.minifyCss())
-    .pipe(cssFilter.restore())
-    .pipe(htmlFilter)
-    .pipe($.htmlmin({collapseWhitespace: true}))
-    .pipe(htmlFilter.restore())
-    .pipe(revAll.revision())
+gulp.task('html', function() {
+  gulp.src('app/index.html')
     .pipe(gulp.dest('dist'))
-    .pipe($.size());
-});
+})
 
-gulp.task('webserver', function() {
-  webserver = gulp.src(['.tmp', 'app'])
-    .pipe($.webserver({
-      host: 'localhost',
-      livereload: {
-        enable: true,
-        filter: function(filePath) {
-          if (/app\\(?=scripts)/.test(filePath)) {
-            $.util.log('Ignoring', $.util.colors.magenta(filePath));
-            return false;
-          } else {
-            return true;
-          }
-        }
-      },
-      open: true
-    }));
-});
+gulp.task('scripts', function() {
+  browserify({entries: './app/scripts/app.js', extensions: ['.jsx']}).transform('reactify')
+    .bundle()
+    .pipe(source('app.min.js'))
+    .pipe(streamify(uglify('app.min.js')))
+    .pipe(gulp.dest('dist'))
+})
 
-gulp.task('start', function() {
-  runSequence('clean:dev', ['scripts']);
-  gulp.watch('app/*.html');
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/scripts/**/*.jsx', ['scripts']);
-});
+gulp.task('build', ['less', 'fonts', 'html', 'scripts'])
 
-gulp.task('build', function() {
-  env = 'prod';
-  runSequence(['clean:dev', 'clean:dist'],
-              ['scripts', 'less', 'imagemin'],
-              'bundle', 'copy');
-});
-
-gulp.task('default', ['build']);
+gulp.task('default', ['build'])
