9541ee9062caf91c27aba2ac8763181fd9a169b2
Krishna Srinivas <krishna.srinivas@gmail.com>
2015-08-29 18:18:51 -0700

    Include JS files for jquery and bootstrap.

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,14 @@ var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
 var rename = require('gulp-rename');
 var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 gulp.task('clean', function() {
   return del(['dist']);
 });
 
 gulp.task('less', function () {
-  gulp.src('./app/styles/less/app.less') //path to your main less file
+  gulp.src('./app/styles/app.less') //path to your main less file
     .pipe(less())
     .pipe(minifycss())
     .pipe(rename('app.min.css'))
@@ -31,11 +32,17 @@ gulp.task('html', function() {
 })
 
 gulp.task('scripts', function() {
-  browserify({entries: './app/scripts/app.js', extensions: ['.jsx']}).transform('reactify')
+  browserify({entries: './app/scripts/minio/app.js', extensions: ['.jsx']}).transform('reactify')
     .bundle()
     .pipe(source('app.min.js'))
     .pipe(streamify(uglify('app.min.js')))
     .pipe(gulp.dest('dist'))
+
+  gulp.src('app/scripts/extern/*.js')
+    .pipe(concat('extern.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+
 })
 
 gulp.task('build', ['less', 'fonts', 'html', 'scripts'])
