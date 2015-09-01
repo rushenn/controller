2f7961c1f2626d1d2d78b1be2ca6f940a55aec51
Krishna Srinivas <krishna.srinivas@gmail.com>
2015-08-31 21:23:05 -0700

    gulpfile.js correctness fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
+/*
+ * Minio Cloud Storage, (C) 2015 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var gulp = require('gulp');
 var del = require('del');
 var source = require('vinyl-source-stream');
@@ -14,7 +30,7 @@ gulp.task('clean', function() {
 });
 
 gulp.task('less', function () {
-  gulp.src('./app/styles/app.less') //path to your main less file
+  return gulp.src('./app/styles/app.less') //path to your main less file
     .pipe(less())
     .pipe(minifycss())
     .pipe(rename('app.min.css'))
@@ -22,28 +38,31 @@ gulp.task('less', function () {
 });
 
 gulp.task('fonts', function() {
-  gulp.src('app/fonts/**/*', {base:"app"})
+  return gulp.src('app/fonts/**/*', {base:"app"})
     .pipe(gulp.dest('dist'))
 })
 
 gulp.task('html', function() {
-  gulp.src('app/index.html')
+  return gulp.src('app/index.html')
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('scripts', function() {
-  browserify({entries: './app/scripts/minio/app.js', extensions: ['.jsx']}).transform('reactify')
+gulp.task('scripts-react', function() {
+  return browserify({entries: './app/scripts/minio/app.js', extensions: ['.jsx']}).transform('reactify')
     .bundle()
     .pipe(source('app.min.js'))
-    .pipe(streamify(uglify('app.min.js')))
+    .pipe(streamify(uglify()))
     .pipe(gulp.dest('dist'))
+})
 
-  gulp.src(['app/scripts/extern/jquery.js', 'app/scripts/extern/bootstrap.js'])
+gulp.task('scripts-others', function() {
+  return gulp.src(['app/scripts/extern/jquery.js', 'app/scripts/extern/bootstrap.js'])
     .pipe(concat('extern.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist'))
-
 })
+
+gulp.task('scripts', ['scripts-react', 'scripts-others'])
 
 gulp.task('build', ['less', 'fonts', 'html', 'scripts'])
 
