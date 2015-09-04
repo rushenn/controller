2d80c592a6294039379767516ce107f7f47a9241
Harshavardhana <harsha@minio.io>
2015-09-03 18:47:58 -0700

    Add notification mockup along with other changes to header.less

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 /*
  * Minio Cloud Storage, (C) 2015 Minio, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -54,12 +54,12 @@ gulp.task('less', function () {
 });
 
 gulp.task('fonts', function() {
-  return gulp.src(paths.fonts, {base:"app"})
+  return gulp.src(paths.fonts, {base:'app'})
     .pipe(gulp.dest('dist'))
 })
 
 gulp.task('images', function() {
-  return gulp.src(paths.images, {base:"app"})
+  return gulp.src(paths.images, {base:'app'})
     .pipe(imagemin())
     .pipe(gulp.dest('dist'))
 })
