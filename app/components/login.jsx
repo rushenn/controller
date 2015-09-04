846f120b9a8234ad3a3c9780462c69e87a0e2c2b
Harshavardhana <harsha@minio.io>
2015-09-03 18:20:16 -0700

    Add proper login home page

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -15,21 +15,14 @@
  */
 
 var React = require('react');
-var Router = require('react-router');
-var Route = Router.Route;
-var DefaultRoute = Router.DefaultRoute;
 
-var Layout = require('../components/layout');
-var Home = require('../components/home');
+var Login = React.createClass({
+  render: function() {
+    return (
+      <div>
+      </div>
+    );
+  }
+});
 
-var routes = (
-  <Route name='layout' path='/' handler={Layout}>
-    <DefaultRoute handler={Home} />
-  </Route>
-);
-
-exports.start = function() {
-  Router.run(routes, function (Handler) {
-    React.render(<Handler />, document.getElementById('login-content'));
-  });
-};
+module.exports = Login;
